f14dfd6b7b57f3de6eb7cee96a92da761e0fc0cb
Marzan Islam <marzanislam999@gmail.com>
2023-01-05 21:55:40 +0600

    done

--- music_app/lib/main.dart
+++ music_app/lib/main.dart
@@ -24,8 +24,8 @@ class MyApp extends StatelessWidget {
       home: const HomeScreen(),
       getPages: [
         GetPage(name: '/', page: () => const HomeScreen()),
-        // GetPage(name: '/song', page: () => const SongScreen()),
-        // GetPage(name: '/playlist', page: () => const PlaylistScreen()),
+        GetPage(name: '/song', page: () => const SongScreen()),
+        GetPage(name: '/playlist', page: () => const PlaylistScreen()),
       ],
     );
   }
